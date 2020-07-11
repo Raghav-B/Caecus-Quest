5d7f12b06990849ff64b361ea69d367b49d77971
Deculsion <unknownkiller100@gmail.com>
2020-07-11 11:02:48 +0800

    Added tile indicators

---
+++ Assets/Player/DevController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevController : MonoBehaviour
+{
+    public GameObject TileIndicator;
+    private TargettedTileController tileIndicatorScript;
+
+    void Awake()
+    {
+        tileIndicatorScript = TileIndicator.GetComponent<TargettedTileController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            float deltaX = Mathf.Clamp( (Input.GetAxis("Horizontal") * 100), -1, 1);
+            float deltaZ = Mathf.Clamp((Input.GetAxis("Vertical") * 100), -1, 1);
+
+            Vector3 targetDelta = new Vector3((int)deltaX, 0f, (int)deltaZ);
+            Debug.Log(targetDelta);
+            tileIndicatorScript.targetDelta = targetDelta;
+        }
+    }
+}
--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -6,6 +6,9 @@ public class MouseController : MonoBehaviour
 {
     public Vector3 worldPosition;
     public Plane plane;
+    public GameObject TileIndicator;
+
+    private TargettedTileController tileIndicatorScript;
     void Update() {
 
         RaycastHit hit;
@@ -13,13 +16,22 @@ public class MouseController : MonoBehaviour
         if (Physics.Raycast(ray, out hit)) {
             worldPosition = hit.point;
 
-            Debug.Log(worldPosition);
+            //Debug.Log(worldPosition);
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            float deltaX = Mathf.Clamp((Input.GetAxis("Horizontal") * 100), -1, 1);
+            float deltaZ = Mathf.Clamp((Input.GetAxis("Vertical") * 100), -1, 1);
+
+            Vector3 targetPos = new Vector3(Mathf.RoundToInt(worldPosition.x), 0f, Mathf.RoundToInt(worldPosition.z));
+            Debug.Log("Target:" + targetPos);
+            tileIndicatorScript.targetLocation = targetPos;
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        tileIndicatorScript = TileIndicator.GetComponent<TargettedTileController>();
     }
 }
