cd53b2e548da759fcc59957b99cf256321716e76
Deculsion <unknownkiller100@gmail.com>
2020-07-11 23:28:17 +0800

    Add test spell effect to effect prefabs. Yet to do anything with it.

--- Assets/Player/Cards/Effects/CardEffect.cs
+++ Assets/Player/Cards/Effects/CardEffect.cs
@@ -6,6 +6,7 @@ public abstract class CardEffect : MonoBehaviour
 {
 
     public float radius = 0.5f;
+    public GameObject SpellEffectContainer;
     public virtual void ExecuteEffect(Collider[] colliders) {
     }
 }
--- Assets/TestSpellFXScript.cs
+++ Assets/TestSpellFXScript.cs
@@ -7,21 +7,23 @@ public class TestSpellFXScript : MonoBehaviour
     public GameObject SpellCube;
     GameObject[] cubes = new GameObject[4];
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         Quaternion rotation = transform.rotation;
         
         for (int i = 0; i < 4; i++) {
-            cubes[i] = Object.Instantiate(SpellCube, transform.position, rotation);
-            rotation.eulerAngles = rotation.eulerAngles + new Vector3(0f, 90f, 0f);
-        }   
+            cubes[i] = Object.Instantiate(SpellCube, transform.position, transform.rotation);
+            transform.Rotate(Vector3.up, 90f);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            Destroy(cubes[i], SpellCube.GetComponentInChildren<Animation>().clip.length);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < 4; i++) {
-            Destroy(cubes[i], SpellCube.GetComponent<Animation>().clip.length);
-        }
+
     }
 }
