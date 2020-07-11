40a69ecc7fa8de718ec640a831540500204e1fe1
Raghav-B <raghavbhardwaj1998@gmail.com>
2020-07-11 15:35:10 +0800

    Added some cards

--- Assets/Player/Cards/Card.cs
+++ Assets/Player/Cards/Card.cs
@@ -6,8 +6,16 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName ="New Card",menuName ="Card")]
 public class Card : ScriptableObject
 {
+    public enum Rarity {
+        COMMON, RARE, EPIC, LEGENDARY
+    }
+
     public int cardID;
     public string cardName;
+    public Rarity rarity;
     public string description;
+    public float chanceToHit;
     public Sprite splashArt;
+
+    public Character character;
 }
