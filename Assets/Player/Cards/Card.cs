feff10622a44c56e3473f5dc2ae0e5c7fe5c46b7
Deculsion <unknownkiller100@gmail.com>
2020-07-11 12:23:19 +0800

    Halfway added cards

---
+++ Assets/Player/Cards/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New Card",menuName ="Card")]
+public class Card : ScriptableObject
+{
+    public int cardID;
+    public string cardName;
+    public string description;
+    public Sprite splashArt;
+}
