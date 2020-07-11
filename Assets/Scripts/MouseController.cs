10fb25855b20c6049ac30765c9f732659e4c81c0
Deculsion <unknownkiller100@gmail.com>
2020-07-11 10:02:49 +0800

    Added mouse raycast

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public Vector3 worldPosition;
+    public Plane plane;
+    void Update() {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            worldPosition = hit.point;
+
+            Debug.Log(worldPosition);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
