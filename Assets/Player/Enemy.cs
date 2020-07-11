2608e3bca48a3051d6383d4c59aa4fcd571b49e6
Deculsion <unknownkiller100@gmail.com>
2020-07-11 18:04:14 +0800

    Added basic enemy

--- Assets/Player/Enemy.cs
+++ Assets/Player/Enemy.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : Character {
-    
-    [SerializeField]
-    private float damage;
-    
-    public Enemy(float startHealth, float startArmor, float damage) {
-        setHealth(startHealth);
-        setArmor(startArmor);
-        this.damage = damage;
-    }
 
-    public void damagePlayer() {
+    public void damagePlayer(int damage) {
         bool isPlayerAlive = Player.getInstance().takeDamage(damage);
         if (!isPlayerAlive) {
             Player.getInstance().die();
         }
-    }
-
-    public override void die() {
     }
 }
