446c8117892a13a1dd59774ff8a576f6e46100f1
Deculsion <unknownkiller100@gmail.com>
2020-07-12 14:55:41 +0800

    Added new cards

---
+++ Assets/Player/Cards/Effects/DamageRandomEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageRandomEffect : CardEffect
+{
+
+    public float damage = 10;
+    public int targets = 1;
+    public override void ExecuteEffect(Collider[] colliders) {
+        List<Enemy> enemies = EnemyController.Instance.GetEnemies();
+        int left = targets;
+        while (left-- > 0) {
+            int randInt = Random.Range(0, enemies.Count);
+
+            enemies[randInt].GetComponent<Enemy>().takeDamage(damage);
+        }
+      
+    }
+}
