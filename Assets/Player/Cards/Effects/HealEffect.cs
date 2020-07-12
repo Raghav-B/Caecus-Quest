464fa75da1d343de3e16b6132f82e11be8afbf51
Deculsion <unknownkiller100@gmail.com>
2020-07-12 13:00:36 +0800

    Some spell effects changed

---
+++ Assets/Player/Cards/Effects/HealEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealEffect : CardEffect
+{
+
+    public bool healPlayer = true;
+    public bool healEnemy = true;
+    public float healAmt = 50;
+
+    public override void ExecuteEffect(Collider[] colliders) {
+        for (int i = 0; i < colliders.Length; i++) {
+            if (healPlayer && colliders[i].tag == "Player") {
+                colliders[i].GetComponent<Character>().heal(healAmt);
+            }
+
+            else if (healEnemy && colliders[i].tag == "Enemy") {
+                colliders[i].GetComponent<Character>().heal(healAmt);
+            }
+        }
+    }
+
+}
