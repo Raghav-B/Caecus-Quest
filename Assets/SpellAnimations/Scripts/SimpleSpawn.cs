2572669968eae42093153c972d9e1b8239dfa118
Deculsion <unknownkiller100@gmail.com>
2020-07-12 16:34:31 +0800

    Explosion effect

--- Assets/SpellAnimations/Scripts/SimpleSpawn.cs
+++ Assets/SpellAnimations/Scripts/SimpleSpawn.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BubbleFXScript : MonoBehaviour
+public class SimpleSpawn : MonoBehaviour
 {
 
-    public GameObject BubbleObject;
+    public GameObject SpawnObject;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject obj = Object.Instantiate(BubbleObject, transform);
+        GameObject obj = Object.Instantiate(SpawnObject, transform);
         Destroy(obj, obj.GetComponentInChildren<Animation>().clip.length);
         Destroy(gameObject, GetComponentInChildren<Animation>().clip.length);
     }
